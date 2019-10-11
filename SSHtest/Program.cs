230036ce593a3b9afafca990d83c9e8b73015d63
Neophear <stiiggade@gmail.com>
2019-10-11 10:44:41 +0200

    Initial commit

---
+++ SSHtest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tamir.SharpSsh;
+
+namespace SSHtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SshShell lol = new SshShell("10.10.99.2", "admin");
+            lol.Password = "XXXX";
+            lol.RedirectToConsole();
+            lol.Connect();
+
+            while (lol.ShellOpened)
+            {
+                System.Threading.Thread.Sleep(500);
+            }
+            lol.Close();
+        }
+    }
+}
